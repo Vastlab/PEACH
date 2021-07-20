f8f78e200ba747f6c6ff28087d2335f13eb0857e
ROBOTICSENGINEER <mohsen.jafarzade@gmail.com>
2021-07-20 13:46:52 -0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+from FACTO import FACTO
+import numpy as np
+import time
+
+t0 = time.time()
+features = np.random.rand(5000,3072)
+
+gpu = 0
+result = FACTO(features, gpu, no_singleton = False) # 0 means GPU0
+
+
+print(result)
+
+t1 = time.time()
+print("runining time = ", t1-t0)
