7819b11f502a2c835a08f8bf3378c8fd841f8018
Chunchun Li <chunchun6198@yahoo.com>
2021-09-03 13:40:44 -0600

    keep "SUM", remove "cosine+euclidean"

--- FACTO.py
+++ FACTO.py
@@ -28,7 +28,7 @@ def euclidean(x, y):
     return distances
 
 
-def FACTO(features, gpu, metric="cosine", batch_size = 4096, no_singleton=False):
+def FACTO(features, gpu, metric="cosine", batch_size = 4096, no_singleton=False, evt=False):
     torch.cuda.set_device(gpu)
     if features.shape[1] > 128:
         try:
@@ -38,15 +38,14 @@ def FACTO(features, gpu, metric="cosine", batch_size = 4096, no_singleton=False)
         except:
             pass
     points = features
+    #print("PCA DIM:", features.shape[1])
     print("FACTO Start")
     length = len(points)
     # Get threshold
-    if metric == "cosine+euclidean":
-        estimated_gap_cos, estimated_gap_eu, nearest_points_cos, nearest_points_eu, init_length_cos, init_length_eu, nearest_cluster_with_distance_round_1_cos, nearest_cluster_with_distance_round_1_eu, nearest_points_dis_cos, nearest_points_dis_eu = tolerance(features, gpu, metric, batch_size)
-    elif metric == "SUM":
-        estimated_gap, nearest_points, init_length, nearest_cluster_with_distance_round_1, nearest_points_dis, max_eu = tolerance(features, gpu, metric, batch_size)
+    if metric == "SUM":
+        estimated_gap, nearest_points, init_length, nearest_cluster_with_distance_round_1, nearest_points_dis, max_eu = tolerance(features, gpu, metric, batch_size, evt)
     else:
-        estimated_gap, nearest_points, init_length, nearest_cluster_with_distance_round_1, nearest_points_dis = tolerance(features, gpu, metric, batch_size)
+        estimated_gap, nearest_points, init_length, nearest_cluster_with_distance_round_1, nearest_points_dis = tolerance(features, gpu, metric, batch_size, evt)
     ################################################################################################
     clusters = [[i] for i in range(length)]
 
@@ -60,56 +59,27 @@ def FACTO(features, gpu, metric="cosine", batch_size = 4096, no_singleton=False)
         if round == 0:
             # In round 1 the centroids is the points no matter what's linkage
             # Merging by nearest points
-            if metric == "cosine+euclidean":
-                nearest_cluster_with_distance_cos = nearest_cluster_with_distance_round_1_cos
-                nearest_cluster_with_distance_eu = nearest_cluster_with_distance_round_1_eu
-                nearest_cluster_cos = []
-                nearest_cluster_eu = []
-                for m in sorted(nearest_cluster_with_distance_cos, key=takeSecond):
-                    nearest_cluster_cos.append(m[1][1])
-                for m in sorted(nearest_cluster_with_distance_eu, key=takeSecond):
-                    nearest_cluster_eu.append(m[1][1])
-                nearest_cluster = []
-                ###############################AND################################
-                processed = set()
-                for a, b in zip(nearest_cluster_cos, nearest_cluster_eu):
-                    if method == "OR":
-                        if a not in processed:
-                            nearest_cluster.append(a)
-                            processed.add(a)
-                        elif b not in processed:
-                            nearest_cluster.append(b)
-                            processed.add(b)
-                    elif method == "AND":
-                        if a == b:
-                            nearest_cluster.append(a)
-                ##################################################################
-                merging_list = set()
-                for m, n in enumerate(nearest_cluster):
-                    merging_list.add(tuple((m, n)))
-
-            else:
-                nearest_cluster_with_distance = nearest_cluster_with_distance_round_1
-                nearest_cluster = []
-                nearest_cluster_dis = []
-                for m in sorted(nearest_cluster_with_distance, key=takeSecond):
-                    nearest_cluster_dis.append(m[0])
-                    nearest_cluster.append(m[1][1])
-                #############################################
-                ###############Generate merging list#########
-                #############################################
-                nearest_cluster_with_distance = sorted(
-                    nearest_cluster_with_distance)  # Sort by distance, process the smallest one first
-                merging_list = set()
-                merging_list_with_cluster_id = set()
-                processed = set()
-                # Generate merging list (cluster pairs to be merged)(sorted by shortest distance)
-                for i, j in enumerate(nearest_cluster_with_distance):
-                    if j[1][0] not in processed and j[1][1] not in processed:
-                        merging_list.add(tuple(j[1]))
-                        merging_list_with_cluster_id.add((tuple(clusters[j[1][0]]), tuple(clusters[j[1][1]])))
-                        processed.add(j[1][0])
-                        processed.add(j[1][1])
+            nearest_cluster_with_distance = nearest_cluster_with_distance_round_1
+            nearest_cluster = []
+            nearest_cluster_dis = []
+            for m in sorted(nearest_cluster_with_distance, key=takeSecond):
+                nearest_cluster_dis.append(m[0])
+                nearest_cluster.append(m[1][1])
+            #############################################
+            ###############Generate merging list#########
+            #############################################
+            nearest_cluster_with_distance = sorted(
+                nearest_cluster_with_distance)  # Sort by distance, process the smallest one first
+            merging_list = set()
+            merging_list_with_cluster_id = set()
+            processed = set()
+            # Generate merging list (cluster pairs to be merged)(sorted by shortest distance)
+            for i, j in enumerate(nearest_cluster_with_distance):
+                if j[1][0] not in processed and j[1][1] not in processed:
+                    merging_list.add(tuple(j[1]))
+                    merging_list_with_cluster_id.add((tuple(clusters[j[1][0]]), tuple(clusters[j[1][1]])))
+                    processed.add(j[1][0])
+                    processed.add(j[1][1])
 
         else:
             centroids = [np.mean([points[j] for j in i], axis=0) for i in clusters]
@@ -155,10 +125,7 @@ def FACTO(features, gpu, metric="cosine", batch_size = 4096, no_singleton=False)
         old_clusters = set()
         for i in clusters:
             old_clusters.add(tuple(i))  # find old (last round) clusters
-        #print("Merging")
-        if metric == "cosine+euclidean":
-            clusters = merging_combine(merging_list, clusters, estimated_gap_cos, estimated_gap_eu, features, round, metric, method)
-        elif metric == "SUM":
+        if metric == "SUM":
             clusters = merging_combine_sum(merging_list, clusters, estimated_gap, features, max_eu)
         else:
             clusters = merging(merging_list, clusters, estimated_gap, features, round, metric)
