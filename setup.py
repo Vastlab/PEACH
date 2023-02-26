50a09069f23cc4db223c8067b292fa7d86b9fc65
tboult <tboult@vast.uccs.edu>
2023-02-25 21:24:00 -0700

    TB update packaging

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='PEACH',
       version='1.0',
       description='Provable Extreme-value Agglomerative Clustering-threshold Estimation',
-      author='Chunchun Li ',
+      author='Chunchun Li & tboult ',
       author_email='cli@vast.uccs.edu',
       url='https://github.com/Vastlab/PEACH/',
       py_modules = ['PEACH', 'tau_flann_pytorch','weibull','merge','evaluate']
