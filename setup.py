f088b7a9083b0fecbe631832553fe744316235c3
Terry Boult <tboult@vast.uccs.edu>
2023-02-25 20:32:02 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='PEACH',
       version='1.0',
       description='Provable Extreme-value Agglomerative Clustering-threshold Estimation',
       author='Chunchun Li ',
-      author_email=cli@vast.uccs.edu',
+      author_email='cli@vast.uccs.edu',
       url='https://github.com/Vastlab/PEACH/',
       packages=['PEACH'],
      )
